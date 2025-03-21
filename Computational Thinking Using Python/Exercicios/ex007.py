56a625e927eac895771825447558379fba6ef66e
matheusmoyaoliveira <matheus.dev.moya@hotmail.com>
2025-03-20 21:22:54 -0300

    Alterando o ex 007

--- Computational Thinking Using Python/Exercicios/ex007.py
+++ Computational Thinking Using Python/Exercicios/ex007.py
@@ -1,4 +1,10 @@
 # Sua tarefa é desenvolver um algoritmo que recebe um número inteiro de 0 a 99 e imprime o dígito das dezenas e o dígito das unidades desse número. Dica: usando papel e lápis faça a divisão inteira do número por 10 mas não coloque vírgula e nem acrescente 0 na divisão.
+
+#recebendo o número
 n1 = int(input('Digite um número inteiro: '))
 
-print('Você digitou o número {:.2f}'.format(n1))
+result1 = (n1 // 10)
+result2 = (n1 % 10)
+
+
+print(f'Você digitou o número {result1:.0f}, {result2:.0f}')
