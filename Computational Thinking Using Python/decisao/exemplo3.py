aa839b0261cce400fba9d4030b3f2a1518b29d37
matheusmoyaoliveira <matheus.dev.moya@hotmail.com>
2025-03-27 21:02:07 -0300

    Comandos de decisão da aula FIAP

---
+++ Computational Thinking Using Python/decisao/exemplo3.py
@@ -0,0 +1,17 @@
+num_a = float(input('Digite um número: '))
+op = input('Digite o operador(+-*/): ')
+num_b = float(input('Digite outro número: '))
+
+if op == '+':
+    resultado = num_a + num_b
+elif op == '-':
+    resultado = num_a - num_b
+elif op == '*':
+    resultado = num_a * num_b
+elif op == '/':
+    resultado = num_a / num_b
+else:
+    print(f'Operador {op} inválido!')
+    resultado = None #vazio ou nada
+
+print(f'{num_a} {op} {num_b} = {resultado}')
