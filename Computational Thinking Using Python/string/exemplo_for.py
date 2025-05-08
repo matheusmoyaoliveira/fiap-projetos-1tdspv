0c8a56d2f6a1e0b537042b2bc13b6cd4053e8a91
matheusmoyaoliveira <matheus.dev.moya@hotmail.com>
2025-05-08 20:16:40 -0300

    Testando caminhos de branchs

---
+++ Computational Thinking Using Python/string/exemplo_for.py
@@ -0,0 +1,10 @@
+frase = '''Eu só enxerguei mais longe porque me apoiei em ombros de gigantes'''
+
+for letra in frase:
+    print(letra)
+
+s = " This is the beginning of anything you want "
+i = 0
+while i < len(s):
+    print(s[i])
+    i += 1
