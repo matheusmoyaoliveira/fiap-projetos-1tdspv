94417e69e3cf936fc766e84fd51210d42bd52845
matheusmoyaoliveira <matheus.dev.moya@hotmail.com>
2025-05-23 21:02:32 -0300

    fim dos exercicios da apostila 10 de java

--- Domain Driven Design using Java/Aulas/Aula10/src/br/com/fiap/loja/model/Alimento.java
+++ Domain Driven Design using Java/Aulas/Aula10/src/br/com/fiap/loja/model/Alimento.java
@@ -2,21 +2,40 @@ package br.com.fiap.loja.model;
 
 import java.time.LocalDate;
 
-public class Alimento extends Produto {
-    private LocalDate vencimento;
+public class Alimento extends Produto{
+
+    private LocalDate dataVencimento;
+
+    public Alimento(int codigo, String descricao, double preco, LocalDate dataVencimento) {
+        super(codigo, descricao, preco);
+        this.dataVencimento = dataVencimento;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Data Vencimento: " + dataVencimento;
+    }
 
     @Override //Sobrescrita do método do pai
-    public double calcularDesconto() {
+    public double calcularDesconto(){
         //Calcular um desconto de 15%
         return calcularDesconto(15);
-
     }
 
-    public LocalDate getVencimento() {
-        return vencimento;
+    @Override
+    public double calcularDesconto(String cupom){
+        //validar o cupom de 40%
+        if (cupom.equals("FIAP40"))
+            return calcularDesconto(40);
+        return super.calcularDesconto(cupom);
     }
 
-    public void setVencimento(LocalDate vencimento) {
-        this.vencimento = vencimento;
+
+    public LocalDate getDataVencimento() {
+        return dataVencimento;
+    }
+
+    public void setDataVencimento(LocalDate dataVencimento) {
+        this.dataVencimento = dataVencimento;
     }
 }
--- Domain Driven Design using Java/Aulas/Aula10/src/br/com/fiap/loja/model/Produto.java
+++ Domain Driven Design using Java/Aulas/Aula10/src/br/com/fiap/loja/model/Produto.java
@@ -5,48 +5,37 @@ public class Produto {
     private int codigo;
     private String descricao;
     private double preco;
-    private String cupom;
 
+    public Produto(int codigo, String descricao, double preco) {
+        this.codigo = codigo;
+        this.descricao = descricao;
+        this.preco = preco;
+    }
+
+    @Override
+    public String toString() {
+        return "Código: " + codigo + ", Descrição: " + descricao + ", Preço: " + preco;
+    }
 
     //Métodos
     //Calcula e retorna o valor com desconto
-    public double calcularDesconto(int porcentagem) {
-        return preco - (preco * porcentagem) / 100f;
+    public double calcularDesconto(int porcentagem){
+        return preco - (preco * porcentagem)/100;
     }
 
     //Calcula o desconto padrão de 5%
-    public double calcularDesconto() {
+    public double calcularDesconto(){
         return calcularDesconto(5);
     }
 
-    //Sobrecarga do método de calcular desconto:
+    //Sobrecarga do método de calcular desconto
     //Recebe uma String (cupom) FIAP10 -> 10%, FIAP20 -> 20%
-    public double calcularDesconto(String cupom) {
-        if (cupom.equals("FIAP10")) {
-            return preco - (preco * 10) / 100f;
-        } else if (cupom.equals("FIAP20")) {
-            return preco - (preco * 20) / 100f;
-        }
-    }
-
-    //Sobrescrita do método na classe Alimento
-    //Recebe uma String (cupom) FIAP10 -> 10%, FIAP20 -> 20%, FIAP40 -> 40%
-    public double calcularDesconto(String cupom) {
-        if (cupom.equals("FIAP10")) {
-            return preco - (preco * 10) / 100f;
-        } else if (cupom.equals("FIAP20")) {
-            return preco - (preco * 20) / 100f;
-        } else if (cupom.equals("FIAP40")) {
-            return preco - (preco * 40) / 100f;
-        }
-    }
-
-    public String getCupom() {
-        return cupom;
-    }
-
-    public void setCupom(String cupom) {
-        this.cupom = cupom;
+    public double calcularDesconto(String cupom){
+        if (cupom.equals("FIAP10"))
+            return calcularDesconto(10);
+        else if (cupom.equals("FIAP20"))
+            return calcularDesconto(20);
+        return preco;
     }
 
     public int getCodigo() {
