a53287ed946820bc8c8d344ae8f64d226c3496f4
matheusmoyaoliveira <matheus.dev.moya@hotmail.com>
2025-08-04 21:31:46 -0300

    feature: Aula de java sobre collections

---
+++ Domain Driven Design using Java/SegundoSemestre/Apostila11/src/br/com/fiap/model/Frutas.java
@@ -0,0 +1,42 @@
+package br.com.fiap.model;
+
+public class Frutas {
+
+    private String nome;
+    private String cor;
+    private double preco;
+
+    public Frutas(String nome, String cor, double preco) {
+        this.nome = nome;
+        this.cor = cor;
+        this.preco = preco;
+    }
+
+    public Frutas() {
+
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
