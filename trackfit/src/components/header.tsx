84eff75a527f45b0407a2db6530f1025971834ab
matheusmoyaoliveira <matheus.dev.moya@hotmail.com>
2025-08-14 00:12:18 -0300

    feat: adicionando novas funcionalidades

---
+++ trackfit/src/components/header.tsx
@@ -0,0 +1,8 @@
+export function Header() {
+    return (
+        <header>
+            <h1>Olá seja bem vindo(a) ao Trackfit</h1>
+            <h2>Aqui você irá realizar treinos insanos</h2>
+        </header>
+    )
+}
