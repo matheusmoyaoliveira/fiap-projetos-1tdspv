381f1434f4b9d43190fde1969298cd1c23eaf1de
matheusmoyaoliveira <matheus.dev.moya@hotmail.com>
2025-08-18 21:01:07 -0300

    feat: pratica de Java

--- Domain Driven Design using Java/SegundoSemestre/Apostila11/src/br/com/fiap/exercicio/model/ProfessorEx.java
+++ Domain Driven Design using Java/SegundoSemestre/Apostila11/src/br/com/fiap/exercicio/model/ProfessorEx.java
@@ -38,7 +38,7 @@ public class ProfessorEx {
         } while(!leitor.next().equalsIgnoreCase("n"));
 
         //Recuperar todos os alunos (valores do mapa)
-        Collection<Aluno> alunos = mapaAluno.values();
+        List<Aluno> alunos = new ArrayList<>(mapaAluno.values().stream().toList());
 
         //Exibir todos os alunos cadastrados
         for (Aluno a : alunos){
@@ -49,13 +49,70 @@ public class ProfessorEx {
         System.out.println("Total de alunos é: " + alunos.size());
 
         int somaIdades = 0;
+        int somaNotas = 0;
+        int maiorIdade = 0;
+        int menorIdade = 0;
         for (Aluno a : alunos){
             somaIdades += a.getIdade();
+            somaNotas += a.getNota1() + a.getNota2();
+            if (a.getIdade() > maiorIdade)
+                maiorIdade = a.getIdade();
+            if (a.getIdade() < menorIdade || menorIdade == 0)
+                menorIdade = a.getIdade();
         }
+
+        //Exibir o aluno com maior e menor idade:
+        System.out.println("Maior idade: " + maiorIdade);
+        System.out.println("Menor idade: " + menorIdade);
+
         //Exibir a média de notas
+        System.out.println("Média de notas dos alunos: " + somaNotas/ (alunos.size()*2));
 
         //Exibir a média de idade
-        System.out.println("Média de idade dos alunos: " + somaIdades/alunos.size());
+        System.out.println("Média de idade dos alunos: " + somaIdades/ alunos.size());
+
+        int opcao;
+        do {
+            System.out.println("Digite a opção: \n1-Exibir nomes e médias " +
+                    "\n2-Alunos com médias maiores que 6" +
+                    "\n3-Alunos com mais de 30 anos \n4-Excluir alunos media < 3\n0-Sair");
+            opcao = leitor.nextInt();
+
+            switch (opcao){
+                case 1:
+                    for(Aluno churros : alunos) {
+                        double media = churros.getNota1() + churros.getNota2() / 2;
+                        System.out.println(churros.getNome() + " " + media);
+                    }
+                    break;
+                case 2:
+                    for (Aluno churros : alunos) {
+                        double media = churros.getNota1() + churros.getNota2() / 2;
+                        if (media > 6) {
+                            System.out.println(churros);
+                        }
+                    }
+                    break;
+                case 3:
+                    for (Aluno churros : alunos) {
+                        double maior = churros.getIdade();
+                        if (maior > 30) {
+                            System.out.println(churros.getNome() + " " + maior);
+                        }
+                    }
+                    break;
+                case 4:
+                    for (int i = 0; i < alunos.size(); i++) {
+                        double media = (alunos.get(i).getNota1() + alunos.get(i).getNota2()) / 2;
+                        if (media < 3) {
+                            alunos.remove(alunos.get(i));
+                            System.out.println("Aluno " + alunos.get(i) + " removido");
+                        }
+                    }
+
+                    break;
+            }
+        } while (opcao != 0);
 
     }//main
 }//class
