05a5edea605c0bd1711818544b416e9d25260916
matheusmoyaoliveira <matheus.dev.moya@hotmail.com>
2025-08-21 19:23:34 -0300

    deletando pasta trackfit

--- trackfit/src/components/header.tsx
+++
@@ -1,8 +0,0 @@
-export function Header() {
-    return (
-        <header>
-            <h1>Olá seja bem vindo(a) ao Trackfit</h1>
-            <h2>Aqui você irá realizar treinos insanos</h2>
-        </header>
-    )
-}
