9606452c500a04ed88e8cea35bfb4b5754ce4d34
matheusmoyaoliveira <matheus.dev.moya@hotmail.com>
2025-09-01 20:55:32 -0300

    começando a conectar em banco de dados

--- Domain Driven Design using Java/SegundoSemestre/Apostila13/src/main/java/br/com/fiap/Main.java
+++ Domain Driven Design using Java/SegundoSemestre/Apostila13/src/main/java/br/com/fiap/Main.java
@@ -1,17 +1,21 @@
 package br.com.fiap;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver"); // registra o driver
+            Connection conn = DriverManager.getConnection(
+                    "jdbc:oracle:thin:@oracle.fiap.com.br:1521:orcl", "rm562822", "130997");
+            System.out.println("Conectado!");
+            conn.close(); // fecha a conexão
+        } catch (ClassNotFoundException e) {
+            System.out.println("O driver JDBC não foi encontrado");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("Não foi possível conectar no banco de dados");
+            e.printStackTrace();
         }
     }
 }
