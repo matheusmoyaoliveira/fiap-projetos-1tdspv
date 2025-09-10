56d0df1e1031121f69e96ee6f45835b7181fe87c
matheusmoyaoliveira <matheus.dev.moya@hotmail.com>
2025-09-09 21:41:11 -0300

    page not found adicionada

--- Front-end Design Engineering/aulas-js/aula1-typescript/trackfit/src/App.tsx
+++ Front-end Design Engineering/aulas-js/aula1-typescript/trackfit/src/App.tsx
@@ -1,20 +1,35 @@
-
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Layout } from "./components/layout";
 import { AddWorkout } from "./pages/add-workout";
 import { Home } from "./pages/home";
 import { WorkoutDetails } from "./pages/workout-details";
+import { NotFound } from "./pages/not-found";
+import { lazy } from "react";
+import { Suspense } from "react";
+import { Loading } from "./components/loading";
+
+const Home = lazy(() => 
+  import('./pages/home').then(m => ({default: m.Home}))
+);
+
+const AddWorkout = lazy(() => 
+  import('./pages/add-workout').then(m => ({default: m.AddWorkout}))
+);
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/add" element={<AddWorkout />} />
-          <Route path="/workout/:id" element={<WorkoutDetails />} />
-        </Route>
-      </Routes>
+
+      <Suspense fallback ={<Loading />}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/add" element={<AddWorkout />} />
+            <Route path="/workout/:id" element={<WorkoutDetails />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   )
 }
