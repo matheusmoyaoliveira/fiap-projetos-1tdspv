c654c14629d04abbff522f71e5f47688a5aa301d
matheusmoyaoliveira <matheus.dev.moya@hotmail.com>
2025-09-17 21:17:57 -0300

    atualizando a develop

--- Front-end Design Engineering/aulas-js/aula1-typescript/trackfit/src/components/workout-resume.tsx
+++ Front-end Design Engineering/aulas-js/aula1-typescript/trackfit/src/components/workout-resume.tsx
@@ -11,13 +11,19 @@ export function WorkoutResume({ workout }: WorkoutResumeProps) {
       <div className="flex justify-between">
         <h2 className="font-bold text-gray-600">{workout.title}</h2>
 
-        <Link
-          to={`/workout/${workout.id}`}
-          state={{ workout }}
-          className="text-blue-600 font-bold hover:underline rounded border border-blue-600 p-2"
-        >
-          detalhes
-        </Link>
+        <div className="flex flex-col gap-2">
+          <Link
+            to={`/workout/${workout.id}`}
+            state={{ workout }}
+            className="text-blue-600 font-bold hover:underline rounded border border-blue-600 p-2"
+          >
+            detalhes
+          </Link>
+
+          <button className="bg-red-600 border text-white rounded p-2">
+            Remover treino
+          </button>
+        </div>
       </div>
 
       <h3>Data: {workout.date}</h3>
--- Front-end Design Engineering/aulas-js/aula1-typescript/trackfit/src/pages/add-workout.tsx
+++ Front-end Design Engineering/aulas-js/aula1-typescript/trackfit/src/pages/add-workout.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { WorkoutForm } from "../components/workout-form";
 import { WorkoutList } from "../components/workout-list";
 import type { Workout } from "../types/workout";
@@ -6,15 +6,38 @@ import type { Workout } from "../types/workout";
 export function AddWorkout() {
   const [workouts, setWorkouts] = useState<Workout[]>([]);
 
-  function addWorkout(workout: Workout): void {
+  const workoutTotal = useMemo(() => {
+    return workouts.length;
+  }, [workouts]);
+
+  const workoutMinutes = useMemo(() => {
+    let workoutMinutes: number = 0;
+
+    workouts.forEach((workout) => {
+      workoutMinutes += workout.duration;
+    });
+
+    const hours = Math.floor(workoutMinutes / 60);
+    const minutes = workoutMinutes % 60;
+
+    return `${hours}:${minutes}`;
+  }, [workouts]);
+
+  const addWorkout = useCallback((workout: Workout) => {
     setWorkouts((prev) => [...prev, workout]);
-  }
+  }, []);
 
   return (
     <>
       <h2 className="font-bold text-blue-800 text-xl mb-3">
         Adicionar novo treino
       </h2>
+
+      <p className="text-lg font-bold text-gray-600">
+        Total de treinos: {workoutTotal}
+      </p>
+
+      <p>Tempo de treino: {workoutMinutes} horas</p>
 
       <WorkoutForm onAdd={addWorkout} />
 
