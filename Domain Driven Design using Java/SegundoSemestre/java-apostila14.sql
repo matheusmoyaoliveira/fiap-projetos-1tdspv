2d7b5c27bd3b1717ea38c3f6327d132dff699a95
matheusmoyaoliveira <matheus.dev.moya@hotmail.com>
2025-09-28 23:39:54 -0300

    atualizandoa postila 4

--- Domain Driven Design using Java/SegundoSemestre/java-apostila14.sql
+++ Domain Driven Design using Java/SegundoSemestre/java-apostila14.sql
@@ -1,8 +1,1 @@
-create table t_tdspv_cliente (
-    cd_cliente number(6,0) primary key,
-    nm_cliente varchar(100),
-    nr_telefone varchar(20),
-    nr_cpf varchar(24),
-    st_ativo number,
-    vl_peso number(5,2)
-)
+select * from t_tdspv_cliente;
