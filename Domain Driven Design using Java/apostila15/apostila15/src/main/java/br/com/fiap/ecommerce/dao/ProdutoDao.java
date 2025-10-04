8ef0e8719f357d6dc550a1fb636e5a3ca3bd0386
matheusmoyaoliveira <matheus.dev.moya@hotmail.com>
2025-10-03 21:09:45 -0300

    Integração com API

---
+++ Domain Driven Design using Java/apostila15/apostila15/src/main/java/br/com/fiap/ecommerce/dao/ProdutoDao.java
@@ -0,0 +1,38 @@
+package br.com.fiap.ecommerce.dao;
+
+import br.com.fiap.ecommerce.model.Produto;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+@ApplicationScoped
+public class ProdutoDao {
+
+    @Inject
+    private DataSource dataSource;
+
+    public void cadastrar(Produto produto) throws SQLException {
+        String sql =
+                "insert into t_tdspv_produto (cd_produto, nm_produto, qt_produto, vl_produto, dt_validade) " +
+                        "values (sq_tdspv_produto.nextval, ?, ?, ?, ?)";
+
+
+        try (Connection conexao = dataSource.getConnection()) {
+            PreparedStatement stmt = conexao.prepareStatement(sql);
+
+            stmt.setString(1, produto.getNome());
+            stmt.setInt(2, produto.getQuantidade());
+            stmt.setDouble(3, produto.getValor());
+            stmt.setObject(4, produto.getDataValidade());
+
+            stmt.executeUpdate();
+
+        }
+    }
+
+}
