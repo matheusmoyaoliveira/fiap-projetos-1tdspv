7f75cb70cca4d38922288df4f4138d8839e3b9eb
matheusmoyaoliveira <matheus.dev.moya@hotmail.com>
2025-10-10 20:57:29 -0300

    refazendo apostila 15

---
+++ Domain Driven Design using Java/apostila15/src/main/java/br/com/fiap/ecommerce/resource/ProdutoResource.java
@@ -0,0 +1,53 @@
+package br.com.fiap.ecommerce.resource;
+
+import br.com.fiap.ecommerce.dao.ProdutoDao;
+import br.com.fiap.ecommerce.exception.EntidadeNaoEncontradaException;
+import br.com.fiap.ecommerce.model.Produto;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.*;
+
+import java.net.URI;
+import java.sql.SQLException;
+import java.util.List;
+
+@Path("/produtos")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class ProdutoResource {
+
+    @Inject
+    private ProdutoDao produtoDao;
+
+    @GET
+    public List<Produto> listar() throws SQLException {
+        return produtoDao.listar();
+    }
+
+    @GET
+    @Path("/{id}")
+    public Response buscar(@PathParam("id") int codigo) throws SQLException, EntidadeNaoEncontradaException {
+        Produto produto = produtoDao.buscar(codigo);
+        return Response.ok(produto).build();
+    }
+
+    @PUT
+    @Path("/{id}")
+    public Response atualizar(@PathParam("id") int codigo, Produto produto) throws SQLException, EntidadeNaoEncontradaException {
+        produto.setCodigo(codigo);
+        produtoDao.atualizar(produto);
+        return Response.ok().build();
+    }
+
+    @POST
+    public Response create(Produto produto, @Context UriInfo uriInfo) throws SQLException {
+        produtoDao.cadastrar(produto);
+
+        //Constroi uma URL de retorno, para acessar o recurso criado
+        URI uri = uriInfo.getAbsolutePathBuilder()
+                .path(String.valueOf(produto.getCodigo())).build();
+
+        return Response.created(uri).entity(produto).build(); //HTTP STATUS CODE 201 (Created)
+    }
+
+}
